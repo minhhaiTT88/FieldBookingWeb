59c7feba5b2fd88e435bd105ab682dd984f1b652
minhhaiTT88 <minhhaiTT88@DESKTOP-6MAEAK0>
2024-07-09 00:30:41 +0700

    hai thong bao dat san

--- src/components/layout/LayoutLeftSideBar.jsx
+++ src/components/layout/LayoutLeftSideBar.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout } from "antd";
 
 import LeftMenu from "../header/LeftMenu";
 import TopControls from "../header/TopControls";
+import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
 
 const LayoutLeftSideBar = (props) => {
+  const processSocket = (e) => {
+    try {
+      if (e?.detail) {
+        if (e.detail.type === "BOOKING_SUCCESS" && e.detail.content) {
+          console.log(e.detail);
+          toast.success(
+            <a href="/quan-ly-don-dat-san">Có đơn đặt sân [{e.detail.content.FieldName}] mới từ khách hàng [{e.detail.content.CustomerName}] khung giờ [{e.detail.content.TimeSlotText}]</a>
+          );
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("socket-message", processSocket);
+    return () => {
+      document.removeEventListener("socket-message", processSocket);
+    };
+  }, []);
+
   return (
     // <div className="relative">
     <Layout className="main-layout h-screen">
